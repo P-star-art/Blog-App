34eadd0b70f2c6211de2ddd949413ef82b4ae8b5
Pranav <prn2108@gmail.com>
2021-02-22 22:44:09 +0530

    feat: added navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import HomePage from './components/HomePage'
+import Navbar from './components/Navbar';
 import './styles/app.css';
 
 const App = () => {
     return (
         <div className="app">
+            <Navbar />
             <HomePage />
         </div>
     )
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,41 +1,46 @@
 import React from 'react';
 import GoogleLogin from 'react-google-login';
-import { useSelector } from 'react-redux';
-import { selectSignedIn } from '../features/userSlice';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectSignedIn, setSignedIn, setUserData } from '../features/userSlice';
 import '../styles/homePage.css';
+import Navbar from './Navbar';
 
 const HomePage = () => {
-
-    const login = (response) => {
-         console.log(response)
-    }
     
     const isSignedIn = useSelector(selectSignedIn);
 
+    const dispatch = useDispatch()
+    const login = (response) => {
+        console.log(response)
+        dispatch(setSignedIn(true));
+        dispatch(setUserData(response.profileObj));
+    }
+
     return (
-        <div className="home__page" style={{display: isSignedIn ? "none" : ""}}>
+        <div className="home__page" style={{ display: isSignedIn ? "none" : "" }}>
             {
-                !isSignedIn ? <div className="login__message">
-                <h2>📗</h2>
-                <h1>A Readers Favourite Place!</h1>
-                <p>We provide high quality online resources from reading blogs. Just sign up and start reading some quality blogs.</p>
-                <GoogleLogin
-                    clientId="1050567203637-l366h5nnoigeo44vu4htlr23ve28kath.apps.googleusercontent.com"
-                    render={(renderProps) => (
-                        <button
-                            onClick={renderProps.onClick}
-                            disabled={renderProps.disabled}
-                            className="login__button"
-                        >
-                            Login with Google
-                        </button>
-                    )}
-                    onSuccess={login}
-                    onFailure={login}
-                    isSignedIn={true}
-                    cookiePolicy={"single_host_origin"}
-                />
-            </div> : <h1>Blogs will be displayed here</h1>
+                !isSignedIn ? (<div className="login__message">
+                    <h2>📗</h2>
+                    <h1>A Readers Favourite Place!</h1>
+                    <p>We provide high quality online resources from reading blogs. Just sign up and start reading some quality blogs.</p>
+                    <GoogleLogin
+                        clientId="1050567203637-l366h5nnoigeo44vu4htlr23ve28kath.apps.googleusercontent.com"
+                        render={(renderProps) => (
+                            <button
+                                onClick={renderProps.onClick}
+                                disabled={renderProps.disabled}
+                                className="login__button"
+                            >
+                                Login with Google
+                            </button>
+                        )}
+                        onSuccess={login}
+                        onFailure={login}
+                        // isSignedIn={true}
+                        cookiePolicy={"single_host_origin"}
+                    />
+                </div>
+                ) : ""
             }
         </div>
     )
